9ea845999c33cf31710dfa9657ff3beac8d3816f
adammendak <adam.mendak@gmail.com>
2017-12-23 19:55:57 +0100

    more methods

--- src/main/java/com/adammendak/todo/controller/ToDoController.java
+++ src/main/java/com/adammendak/todo/controller/ToDoController.java
@@ -5,10 +5,13 @@ import com.adammendak.todo.repository.ToDoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,9 +24,21 @@ public class ToDoController {
         this.toDoRepository = toDoRepository;
     }
 
-    @RequestMapping("/toDo")
+    @GetMapping("/toDo")
     public ResponseEntity<List<ToDo>> getAll() {
         log.info("getting all ToDo's");
         return new ResponseEntity<List<ToDo>>(toDoRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/toDo/{id}")
+    public ResponseEntity<ToDo> getToDo(@PathVariable Long id) {
+        Optional<ToDo> optionalToDo = toDoRepository.findOneById(id);
+        if(optionalToDo.isPresent()) {
+            log.info("getting ToDo with id {}", id);
+            return new ResponseEntity<ToDo>(optionalToDo.get(), HttpStatus.OK)
+        } else {
+            log.info("no such toDo");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
