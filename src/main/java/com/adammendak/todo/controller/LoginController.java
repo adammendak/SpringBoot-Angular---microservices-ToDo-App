ef432f2c84d9f3f5f9ab044f82b768896bf04af3
adammendak <adam.mendak@gmail.com>
2017-12-26 18:01:37 +0100

    switching to JsonResponseBody

--- src/main/java/com/adammendak/todo/controller/LoginController.java
+++ src/main/java/com/adammendak/todo/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.adammendak.todo.controller;
 import com.adammendak.todo.exception.UserNotInDatabaseException;
 import com.adammendak.todo.model.User;
 import com.adammendak.todo.service.LoginService;
+import com.adammendak.todo.utility.JsonResponseBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +25,8 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login (@RequestParam(value = "email") String email,
-                                       @RequestParam(value = "password") String password) {
+    public ResponseEntity<JsonResponseBody> login (@RequestParam(value = "email") String email,
+                                                   @RequestParam(value = "password") String password) {
         User user = null;
         try{
             Optional<User> userOptional =loginService.getUserFromDB(email, password);
@@ -33,13 +34,13 @@ public class LoginController {
             log.info("user from db {}", user.toString());
             String jwt = loginService.createJwt(email,user.getEmail(), new Date());
             log.info("jwt {}", jwt);
-            return ResponseEntity.status(HttpStatus.OK).header("jwt", jwt).body(user);
+            return ResponseEntity.status(HttpStatus.OK).header("jwt", jwt).body(new JsonResponseBody(HttpStatus.OK.value(), "login success!"));
         } catch (UserNotInDatabaseException e) {
             log.info("exception {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(user);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new JsonResponseBody(HttpStatus.FORBIDDEN.value(), "wrong user!"));
         } catch (UnsupportedEncodingException e) {
             log.info("exception {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new JsonResponseBody(HttpStatus.BAD_REQUEST.value(), "bad request"));
         }
     }
 }
